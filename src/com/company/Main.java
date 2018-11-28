3c2d0f7e88cead8cf8cd99f9fc60387da90de4c7
gramsey1 <graceramsey312@yahoo.com>
2018-11-28 11:14:04 -0500

    eden

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,20 @@
 package com.company;
-
+import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
-            double randomDouble = Math.random();
-            randomDouble = randomDouble * 100 + 1;
-            int randomInt = (int) randomDouble;
-            System.out.println(randomInt);
+        int r = 0, count = 0;
+        Random rndm = new Random();
+        for (int j = 0; j < 100; j++) { //for loop to generate 100 random numbers
+            r = 1 + rndm.nextInt(100);
+            System.out.print(r + " ");
+            count++; //used for the if statement
+            if (count > 100) { //printing out all 100 numbers on one line
+                System.out.println(" ");
+            }
+     
+
+            }
         }
     }
 }
