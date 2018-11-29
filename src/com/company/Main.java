025b19541d2d5411626c5e04e37942c07536cac7
gramsey1 <graceramsey312@yahoo.com>
2018-11-29 17:57:01 -0500

    la

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,34 @@
 package com.company;
-import java.util.Random;
+
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        int r = 0, count = 0;
-        boolean[] array = new boolean[100];
-        array[59]=true;
-        int p = 0;
-        Random rndm = new Random();
-        for (int j = 0; j < array.length; j++) { //for loop to generate 100 random numbers
-            r = 1 + rndm.nextInt(100);
-            System.out.print(r + " ");
-            count++; //used for the if statement
-            if (count > 100) { //printing out all 100 numbers on one line
-                System.out.println(" ");
-            }
-            for (int i = 0; i < array.length; i++) {
-                
-                if (array[i] == true){
+
+        int count=0; int timesRunning=0;
+        System.out.println("Enter the amount you want to run it: Only integers, and don't put a number too big");
+        Scanner kbinput = new Scanner(System.in);
+        int control = kbinput.nextInt();
+        for (int i=0; i<=control; i++){
+            timesRunning++;
+            boolean array[]= new boolean[100];//duh, this does the below here.
+            boolean yes;
+            while(true){
+                Random rndm = new Random();
+                int x = rndm.nextInt(100);//limits what random number is 1-100
+                array[x]=true;
+                count++;
+                yes=true;
+                for(int k=0; k<100; k++){
+                    if(array[k]==false)
+                        yes=false;//this runs the loop again
+                }
+                if(yes==true){
                     break;
                 }
             }
         }
+        int avg = count/timesRunning;
+        System.out.println(avg);
     }
 }
