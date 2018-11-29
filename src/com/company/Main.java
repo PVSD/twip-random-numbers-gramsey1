6d1d595f34c6058e3a94fdf00557d4a4aae3f3a3
gramsey1 <graceramsey312@yahoo.com>
2018-11-28 20:09:47 -0500

    la

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,16 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
         int r = 0, count = 0;
+        boolean[] array = new boolean[100];
+        array[59]=true;
+        int p = 0;
         Random rndm = new Random();
-        for (int j = 0; j < 100; j++) { //for loop to generate 100 random numbers
+        for (int j = 0; j < array.length; j++) { //for loop to generate 100 random numbers
             r = 1 + rndm.nextInt(100);
             System.out.print(r + " ");
             count++; //used for the if statement
             if (count > 100) { //printing out all 100 numbers on one line
                 System.out.println(" ");
             }
-     
-
+            for (int i = 0; i < array.length; i++) {
+                
+                if (array[i] == true){
+                    break;
+                }
             }
         }
     }
